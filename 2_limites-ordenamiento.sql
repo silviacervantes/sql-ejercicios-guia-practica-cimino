8c399ccdf149e1e5ec1bb48c1c240a0577e4ce67
Silvia Cervantes <silviaccp@gmail.com>
2022-06-01 11:03:58 -0300

    2_limites-ordenamiento

---
+++ 2_limites-ordenamiento.sql
@@ -0,0 +1,6 @@
+# 6. Listar toda la información de los países, ordenados por población de manera ascendente. (Se esperan 15 columnas y 239 registros).
+
+# 7. Listar nombre de los lenguajes en orden alfabético. (Se espera 1 columna y 984 registros).
+# 8. Listar nombre y cantidad de habitantes de las veinte ciudades menos pobladas. (Se esperan 2 columnas y 20 registros).
+# 9. Listar código, nombre y año de independencia de todos los países, ordenados por antigüedad descendente. (Se esperan 3 columnas y 239 registros).
+# 10. Listar nombre y continente de los cien países con mayor expectativa de vida. Si hubiera países que tengan la misma expectativa de vida, mostrar primero a los de menor superficie. (Se esperan 2 columnas y 100 registros).
