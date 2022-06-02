d2e6e403a31d38c643dac05e696dbb73de57fc4a
Silvia Cervantes <silviaccp@gmail.com>
2022-06-02 11:39:38 -0300

    union

--- 5_union.sql
+++ 5_union.sql
@@ -18,12 +18,39 @@ WHERE country.Population > 1000000 AND Population < 3000000;
 SELECT country.code AS CODIGO,country.localname AS "NOMBRE LOCAL",country.region AS REGION
 FROM countrylanguage
 RIGHT JOIN country ON country.code = countrylanguage.CountryCode
-WHERE countrylanguage.Language='Spanish'
+WHERE countrylanguage.Language='Spanish';
 # 31. Listar los nombres y las capitales de los países en cuya capital se concentre más de la mitad de 
 #su población total. (Se esperan 2 columnas y 14 registros).
+SELECT city.name, country.name 
+FROM city 
+RIGHT JOIN country ON city.id = country.capital 
+WHERE city.population >= (country.population/2);
+# 32. Listar los nombres y la superficie de los países africanos cuya capital coincida con el nombre del
+# distrito a la que pertenece. (Se esperan 2 columnas y 32 registros).
+SELECT P.name AS PAIS, P.SurfaceArea AS SUPERFICIE, C.name AS nombre, C.district AS DISTRITO
+FROM country AS P 
+LEFT JOIN city AS C ON c.id = P.capital 
+WHERE P.continent = 'Africa' AND C.name = C.district ;
+# 33. Listar los nombres, las capitales y el año de independencia (sin nulos) de los 20 países más 
+# antiguos. (Se esperan 3 columnas y 20 registros).
+SELECT P.name AS PAIS, C.name, P.IndepYear AS anio_indep
+FROM country as P 
+LEFT JOIN city AS C ON P.capital = C.id
+WHERE P.IndepYear is not null 
+ORDER By IndepYear ASC LIMIT 20;
 
+# 34. Listar las ciudades junto a sus idiomas oficiales, donde no se hable español, inglés, portugués, 
+# italiano, francés o alemán de manera oficial. (Se esperan 2 columnas y 2694 registros).
+SELECT C.name AS CIUDAD, L.Language
+FROM city AS C
+LEFT JOIN countrylanguage AS L ON L.CountryCode = C.CountryCode 
+WHERE L.IsOfficial = True AND L.Language not in ('Spanish','English','Portuguese','Italian','French','German');
 
-# 32. Listar los nombres y la superficie de los países africanos cuya capital coincida con el nombre del distrito a la que pertenece. (Se esperan 2 columnas y 32 registros).
-# 33. Listar los nombres, las capitales y el año de independencia (sin nulos) de los 20 países más antiguos. (Se esperan 3 columnas y 20 registros).
-# 34. Listar las ciudades junto a sus idiomas oficiales, donde no se hable español, inglés, portugués, italiano, francés o alemán de manera oficial. (Se esperan 2 columnas y 2694 registros).
-# 35. Listar nombre, población y país de las diez ciudades europeas de habla inglesa más pobladas. (Se esperan 3 columnas y 10 registros).
+# 35. Listar nombre, población y país de las diez ciudades europeas de habla inglesa más pobladas. 
+# (Se esperan 3 columnas y 10 registros).
+SELECT C.name AS CIUDAD, C.population as POBLACION, P.name AS PAIS
+FROM city AS C
+LEFT JOIN countrylanguage AS L ON L.countrycode = C.countrycode
+LEFT JOIN country AS P ON P.code = C.countrycode
+WHERE L.Language = 'English' 
+ORDER By C.population DESC LIMIT 10
