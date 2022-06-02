ae43a2367f95e35d6fc355be848efbf9bbd0a188
Silvia Cervantes <silviaccp@gmail.com>
2022-06-02 08:52:38 -0300

    Update 2_limites-ordenamiento.sql

--- 2_limites-ordenamiento.sql
+++ 2_limites-ordenamiento.sql
@@ -6,9 +6,5 @@ SELECT language FROM countrylanguage ORDER By language ASC;
 SELECT name, population FROM city ORDER By population ASC LIMIT 20; 
 # 9. Listar código, nombre y año de independencia de todos los países, ordenados por antigüedad descendente. (Se esperan 3 columnas y 239 registros).
 SELECT code,name,indepyear FROM country ORDER By indepyear DESC;
-
-# 10. Listar nombre y continente de los cien países con mayor expectativa de vida. Si hubiera países que 
-# tengan la misma expectativa de vida, mostrar primero a los de menor superficie. 
-# (Se esperan 2 columnas y 100 registros).
-
+# 10. Listar nombre y continente de los cien países con mayor expectativa de vida. Si hubiera países que  tengan la misma expectativa de vida, mostrar primero a los de menor superficie.  (Se esperan 2 columnas y 100 registros).
 SELECT name, continet FROM country ORDER By
