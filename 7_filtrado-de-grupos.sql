51a4a75311a3985452ab9cad248457fcb2f12686
Silvia Cervantes <silviaccp@gmail.com>
2022-06-02 13:25:31 -0300

    Create 7_filtrado-de-grupos.sql

---
+++ 7_filtrado-de-grupos.sql
@@ -0,0 +1,15 @@
+# 46. Mostrar listados los años de independencia (sin nulos) junto a la cantidad de países que la hayan 
+# conseguido en ese año. Se desea visualizar aquellos años donde más de un país se haya independizado. 
+# (Se esperan 2 columnas y 39 registros).
+
+# 47. Listar los países junto a la cantidad de idiomas diferentes hablados, pero solo aquellos donde se 
+# hablen entre tres y cinco idiomas diferentes. (Se esperan 2 columnas y 80 registros).
+
+# 48. Mostrar los distritos, junto al nombre del país al que pertenecen, cuya población total 
+# (también listada) no supere los diez mil habitantes. (Se esperan 3 columnas y 35 registros).
+
+# 49. Mostrar las regiones junto a su densidad poblacional promedio, donde ésta supere a la mitad de la 
+# densidad poblacional máxima. (Se esperan 2 columnas y 3 registros).
+
+# 50. Mostrar los lenguajes oficiales junto a su porcentaje promedio de habla, cuyo promedio no supere un 
+# dígito entero. (Se esperan 2 columnas y 7 registros).
